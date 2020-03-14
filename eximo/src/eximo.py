e9b02f11f0fa7355e738632e478f15be5de43929
Bernardo Santos <bernardosantos670@gmail.com>
2020-03-14 00:19:10 +0000

    Separated code, pvp almost done

--- eximo/src/eximo.py
+++ eximo/src/eximo.py
@@ -1,48 +1,20 @@
-from copy import copy
 from colorama import Fore, Back, Style 
+from copy import copy
 
-class State:
-    score = {}
+from state import *
+from aux import *
 
-    def __init__(self, board, player, s1, s2):
-        self.board = board
-        self.player = player
-        self.score[1] = s1
-        self.score[2] = s2
-
-    def print(self):
-        print('     A    B    C    D    E    F    G    H')
-        print('    ---------------------------------------')
-        for row in range(len(self.board)):
-            print(' ' + str(row) + ' | ', end="")
-            for piece in self.board[row]:
-                if piece == 1:
-                    print (Back.RED + '  ', end="")
-                    print(Style.RESET_ALL, end="")
-                elif piece == 2:
-                    print (Back.BLUE + '  ', end="")
-                    print(Style.RESET_ALL, end="")
-                else:
-                    print ('  ', end="")
-                print (' | ', end="")
-            print('\n' + '   | --   --   --   --   --   --   --   --')
-
-
-def add(t1: tuple, t2: tuple) -> tuple:
-    return (t1[0] + t2[0], t1[1] + t2[1])
-def mult(t: tuple, f: int) -> tuple:
-    return (t[0] * f, t[1] * f)
 
 class Eximo:
     start_state = State([
-        [0, 1, 1, 1, 1, 1, 1, 0],
-        [0, 1, 1, 1, 1, 1, 1, 0], 
-        [0, 1, 1, 0, 0, 1, 1, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 2, 2, 0, 0, 2, 2, 0], 
+        [0, 2, 2, 2, 2, 2, 2, 0],
         [0, 2, 2, 2, 2, 2, 2, 0], 
-        [0, 2, 2, 2, 2, 2, 2, 0]], 1, 16, 16)
+        [0, 2, 2, 0, 0, 2, 2, 0], 
+        [0, 0, 1, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 1, 1, 0, 0, 1, 1, 0], 
+        [0, 1, 1, 1, 1, 1, 1, 0], 
+        [0, 1, 1, 1, 1, 1, 1, 0]], 1, 16, 16, 1)
     player = {}
 
     def __init__(self, p1: str, p2: str):
@@ -53,27 +25,142 @@ class Eximo:
         state = self.start_state
         while not self.game_over(state):
             state.print()
+            print(Fore.GREEN + 'Player ' + str(state.player) + "\'s turn")
+            print(Style.RESET_ALL, end="")
+            state = self.player_move(state)
+            state.print()
+            pieces = self.check_last_row(state)
+            while pieces > 0:
+                state = self.sel_dropzone(state)
+                pieces -= 1
+            state = self.change_player(state)
+                
+    def change_player(self, state: State) -> State:
+        if state.moves > 0:
+            return state
+        state.moves = 1
+        state.player = state.player % 2 + 1
+        return state
 
-            self.select_cell()
-        
     def game_over(self, state: State) -> bool:
-        if self.player[1][2] == 0:
+        if state.score[1] == 0:
             print("player 2 won")
             return True
-        elif self.player[2][2] == 0:
+        elif state.score[2] == 0:
             print("player 1 won")
             return True
         return False
+            
+    def player_move(self, state: State) -> State:
+        # TODO: check if the player needs to capture enemy pieces
 
-    def select_cell(self) -> tuple:
-        row = 0
-        col = 0
-        while row < 1 or row > 8:
+        while True:
+            pos = self.sel_piece(state)
+            dir = self.sel_direction()
+            
+            if self.can_move(state, pos, dir):
+                return self.move(state, pos, dir)
+            elif self.can_jump(state, pos, dir):
+                return self.jump_combo(state, pos, dir)
+            elif self.can_capture(state, pos, dir):
+                return self.capture_combo(state, pos, dir)
+    
+    def sel_piece(self, state: State) -> tuple:
+        while True:
             row = int(input('Row (1-8): '))
-        while col < 1 or col > 8:
-            col = ord(input('Col (A-H): ').lower()) - 96
-        return (row, col)
+            col = ord(input('Col (A-H): ').lower()) - 97
 
+            if self.valid_position((row, col)) and self.is_players(state, (row, col)):
+                return (row, col)
+
+    def sel_direction(self) -> tuple:
+        while True:
+            dir = input('Direction (W|NW|N|NE|E): ').upper()
+
+            if dir == 'W':
+                return Direction.WEST
+            elif dir == 'NW':
+                return Direction.NORTHWEST
+            elif dir == 'N':
+                return Direction.NORTH
+            elif dir == 'NE':
+                return Direction.NORTHEAST
+            elif dir == 'E':
+                return Direction.EAST
+
+    def sel_dropzone(self, state: State) -> State:
+        row_c = 2 if (state.player == 1) else 7
+        while True:
+            row = int(input('Row (1-8): '))
+            col = ord(input('Col (A-H): ').lower()) - 97
+            pos = (row, col)
+
+            if (pos >= (row_c, 1) and pos <= (row_c, 6)) or (pos >= (row_c - 1, 1) and pos <= (row_c - 1, 6)) and self.is_empty(state, pos):
+                return self.place_piece(state, pos, state.player)
+
+    
+    def jump_combo(self, state: State, pos: tuple, vec: tuple) -> State:
+        n_state = self.jump(state, pos, vec)
+        n_state.print()
+            
+        dir = self.get_direction(state)
+        n_pos = add(pos, mult(vec, dir * 2))
+
+        if not (self.can_jump(state, pos, Direction.NORTHWEST) or
+                self.can_jump(state, pos, Direction.NORTH) or
+                self.can_jump(state, pos, Direction.NORTHEAST)):
+            n_state.moves -= 1
+            return n_state
+        
+        dir = self.sel_direction()
+        while not self.can_jump(n_state, n_pos, dir):
+            dir = self.sel_direction()
+
+        return self.jump_combo(n_state, n_pos, dir)
+
+    def capture_combo(self, state: State, pos: tuple, vec: tuple) -> State:
+        n_state = self.capture(state, pos, vec)
+        n_state.print()
+            
+        dir = self.get_direction(state)
+        n_pos = add(pos, mult(vec, dir * 2))
+
+        if not (self.can_capture(state, pos, Direction.WEST) or 
+                self.can_capture(state, pos, Direction.NORTHWEST) or 
+                self.can_capture(state, pos, Direction.NORTH) or 
+                self.can_capture(state, pos, Direction.NORTHEAST) or
+                self.can_capture(state, pos, Direction.EAST)):
+            n_state.moves -= 1
+            return n_state
+        
+        dir = self.sel_direction()
+        while not self.can_capture(n_state, n_pos, dir):
+            dir = self.sel_direction()
+
+        return self.capture_combo(n_state, n_pos, dir)
+
+    # checks if the player has any piece in the last row, removes 
+    # those pieces and returns the number of pieces removed
+    def check_last_row(self, state: State) -> int:
+        ret = 0
+        row = 0 if (state.player == 1) else 7
+        for col in range(len(state.board[row])):
+            if state.player == state.board[row][col]:
+                self.remove_piece(state, (row, col))
+                ret += 1
+        state.moves += ret
+        return ret  
+
+    def check_capture(self, state: State) -> bool:
+        for row in range(len(state.board)):
+            for col in range(len(state.board[row])):
+                if not self.is_players(state, (row, col)):
+                    continue
+
+                if self.can_capture(state, (row, col), Direction.WEST) or self.can_capture(state, (row, col), Direction.NORTHWEST) or self.can_capture(state, (row, col), Direction.NORTH) or self.can_capture(state, (row, col), Direction.NORTHEAST) or self.can_capture(state, (row, col), Direction.EAST):
+                    return True
+        return False
+    
     def get_piece(self, state: State, pos: tuple) -> int:
         return state.board[pos[0]][pos[1]]
 
@@ -98,14 +185,23 @@ class Eximo:
     # returns the movement direction multiplier 
     def get_direction(self, state: State) -> int:
         if state.player == 2:
-            return -1
-        return 1
+            return 1
+        return -1
     
     def valid_position(self, pos: tuple) -> bool:
         return pos[0] >= 0 and pos[0] <= 7 and pos[1] >= 0 and pos[1] <= 7
 
 
     # ORDINARY MOVE OPERATORS
+    def can_move(self, state: State, pos: tuple, vec: tuple) -> bool:
+        dir = self.get_direction(state)
+        n_pos = add(pos, mult(vec, dir))
+        print(n_pos)
+        if not self.valid_position(n_pos) or not self.is_empty(state, n_pos):
+            return False
+        
+        return True
+
     def move(self, state: State, pos: tuple, vec: tuple) -> State:
         if not self.is_players(state, pos):
             return state
@@ -121,7 +217,8 @@ class Eximo:
         n_state = copy(state)
         self.place_piece(n_state, pos, 0)
         self.place_piece(n_state, n_pos, state.player)
-        
+        n_state.moves -= 1
+
         return n_state
 
     def move_north(self, state: State, pos: tuple) -> State:
@@ -132,6 +229,19 @@ class Eximo:
         return self.move(state, pos, (1, 1))
 
     # JUMP MOVE OPERATORS
+    def can_jump(self, state: State, pos: tuple, vec: tuple) -> bool:
+        dir = self.get_direction(state)
+
+        t_pos = add(pos, mult(vec, dir))
+        if not self.valid_position(t_pos) or not self.is_players(state, t_pos):
+            return False
+        
+        n_pos = add(pos, mult(vec, dir * 2))
+        if not self.valid_position(n_pos) or not self.is_empty(state, n_pos):
+            return False
+        
+        return True
+
     def jump(self, state: State, pos: tuple, vec: tuple) -> State:
         if not self.is_players(state, pos):
             return state
@@ -160,9 +270,22 @@ class Eximo:
         return self.jump(state, pos, (1, 1))
 
     # CAPTURE OPERATORS
+    def can_capture(self, state: State, pos: tuple, vec: tuple) -> bool:
+        dir = self.get_direction(state)
+
+        t_pos = add(pos, mult(vec, dir))
+        if not self.valid_position(t_pos) or not self.is_opponents(state, t_pos):
+            return False
+        
+        n_pos = add(pos, mult(vec, dir * 2))
+        if not self.valid_position(n_pos) or not self.is_empty(state, n_pos):
+            return False
+        
+        return True
+
     def capture(self, state: State, pos: tuple, vec: tuple) -> State:
-        if not self.is_players(state, pos):
-            return state
+        # if not self.is_players(state, pos):
+        #     return state
 
         dir = self.get_direction(state)
 
@@ -191,8 +314,4 @@ class Eximo:
         return self.capture(state, pos, (0, 1))
     def capture_north_west(self, state: State, pos: tuple) -> State:
         return self.capture(state, pos, (1, 1))
-
-
-game = Eximo('P', 'P')
-game.capture_north(game.start_state, (1, 3)).print()
 
---
+++ eximo/src/main.py
@@ -0,0 +1,8 @@
+from eximo import Eximo
+
+
+game = Eximo('P', 'P')
+# print(game.sel_piece(game.start_state))
+game.play()
+# print(game.sel_direction())
+# print(game.can_move(game.start_state, (5, 2), (1, 0)))
