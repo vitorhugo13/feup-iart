dda237404ffba8f7c0d714b54903b94bc10a7246
João Nuno Matos <joao.nuno.matos@fe.up.pt>
2020-03-28 19:58:53 +0000

    Refactored state out of eximo

--- eximo/src/eximo.py
+++ eximo/src/eximo.py
@@ -1,29 +1,20 @@
 from colorama import Fore, Back, Style 
 from copy import copy
 
-from state import *
-from aux import *
+from state import State, start_state
+from aux import Direction
+import sys
 
 
 class Eximo:
-    start_state = State([
-        [0, 2, 2, 2, 2, 2, 2, 0],
-        [0, 2, 2, 2, 2, 2, 2, 0], 
-        [0, 2, 2, 0, 0, 2, 2, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0, 0, 0, 0], 
-        [0, 1, 1, 0, 0, 1, 1, 0], 
-        [0, 1, 1, 1, 1, 1, 1, 0], 
-        [0, 1, 1, 1, 1, 1, 1, 0]], 1, 16, 16, Start())
-    player = {}
-
     def __init__(self, p1: str, p2: str):
+        self.player = {}
         self.player[1] = p1
         self.player[2] = p2
 
     def play(self):
-        state = self.start_state
-        while not self.game_over(state):
+        state = start_state
+        while not state.game_over():
             state.print()
 
             if self.player[state.player] == 'P':
@@ -33,24 +24,17 @@ class Eximo:
                 state = self.minimax(state, 3, state.player, True)
                 print("------- MINIMAX END --------")
 
-    def game_over(self, state: State) -> bool:
-        if state.score[1] <= 0:
-            print("player 2 won")
-            return True
-        elif state.score[2] <= 0:
-            print("player 1 won")
-            return True
-        return False
-            
-    def sel_cell(self) -> tuple:
+    @staticmethod
+    def sel_cell() -> tuple:
         while True:
             row = int(input('Row (1-8): ')) - 1
             col = ord(input('Col (A-H): ').lower()) - 97
 
-            if self.valid_position((row, col)):
+            if State.valid_position((row, col)):
                 return (row, col)
 
-    def sel_cp_dir(self) -> tuple:
+    @staticmethod
+    def sel_cp_dir() -> tuple:
         while True:
             dir = input('Direction (W|NW|N|NE|E): ').upper()
             if dir == 'W':
@@ -64,7 +48,8 @@ class Eximo:
             elif dir == 'E':
                 return Direction.EAST
 
-    def sel_mv_dir(self) -> tuple:
+    @staticmethod
+    def sel_mv_dir() -> tuple:
         while True:
             dir = input('Direction (NW|N|NE): ').upper()
             if dir == 'NW':
@@ -74,264 +59,42 @@ class Eximo:
             elif dir == 'NE':
                 return Direction.NORTHEAST
 
-    def in_last_row(self, state, pos: tuple) -> bool:
-        return (state.player == 1 and pos[0] == 0) or (state.player == 2 and pos[0] == 7)
-
-    def check_capture(self, state: State) -> bool:
-        dir = state.move_direction()
-        for row in range(len(state.board)):
-            for col in range(len(state.board[row])):
-                if not state.is_ally((row, col)):
-                    continue
-
-                if (self.can_capture(state, dir, (row, col), Direction.WEST) or 
-                    self.can_capture(state, dir, (row, col), Direction.NORTHWEST) or 
-                    self.can_capture(state, dir, (row, col), Direction.NORTH) or 
-                    self.can_capture(state, dir, (row, col), Direction.NORTHEAST) or 
-                    self.can_capture(state, dir, (row, col), Direction.EAST)):
-                    return True
-        return False
-    
-    def valid_position(self, pos: tuple) -> bool:
-        return pos[0] in range(0, 8) and pos[1] in range(0, 8)
-
-    # ORDINARY MOVE OPERATORS
-    def move(self, state: State, pos: tuple, vec: tuple) -> State:
-        if not state.is_ally(pos):
-            return None
-        
-        dir = state.move_direction()
-        n_pos = add(pos, mult(vec, dir))
-
-        if not self.valid_position(n_pos):
-            return None
-        if not state.is_empty(n_pos):
-            return None
-        
-        n_state = state.copy()
-        n_state.remove_piece(pos)
-
-        if self.in_last_row(state, n_pos):
-            self.enter_place_mode(n_state)
-        else:
-            n_state.place_piece(n_pos, state.player)
-            n_state.next_turn()
-
-        return n_state
-
-
-    # JUMP MOVE OPERATORS
-    def can_jump(self, state: State, dir: int, pos: tuple, vec: tuple) -> bool:
-        t_pos = add(pos, mult(vec, dir))
-        if not self.valid_position(t_pos) or not state.is_ally(t_pos):
-            return False
-        
-        n_pos = add(pos, mult(vec, dir * 2))
-        if not self.valid_position(n_pos) or not state.is_empty(n_pos):
-            return False
-        
-        return True
-
-    def jump(self, state: State, pos: tuple, vec: tuple) -> State:
-        if not state.is_ally(pos):
-            return None
-
-        dir = state.move_direction()
-
-        t_pos = add(pos, mult(vec, dir))
-        if not self.valid_position(t_pos) or not state.is_ally(t_pos):
-            return None
-        
-        n_pos = add(pos, mult(vec, dir * 2))
-        if not self.valid_position(n_pos) or not state.is_empty(n_pos):
-            return None
-
-        n_state = state.copy()
-        n_state.remove_piece(pos)
-
-        if self.in_last_row(state, n_pos):
-            self.enter_place_mode(n_state)
-        else:
-            n_state.place_piece(n_pos, state.player)
-            if  not (self.can_jump(n_state, dir, n_pos, Direction.NORTHWEST) or 
-                self.can_jump(n_state, dir, n_pos, Direction.NORTH) or 
-                self.can_jump(n_state, dir, n_pos, Direction.NORTHEAST)):
-                n_state.next_turn()
-            else:
-                n_state.action = Jump(n_pos)
-
-        return n_state
-
-    # CAPTURE OPERATORS
-    def can_capture(self, state: State, dir: int, pos: tuple, vec: tuple) -> bool:
-        t_pos = add(pos, mult(vec, dir))
-        if not self.valid_position(t_pos) or not state.is_enemy(t_pos):
-            return False
-        
-        n_pos = add(pos, mult(vec, dir * 2))
-        if not self.valid_position(n_pos) or not state.is_empty(n_pos):
-            return False
-        
-        return True
-
-    def capture(self, state: State, pos: tuple, vec: tuple) -> State:
-        if not state.is_ally(pos):
-            return None
-        
-        dir = state.move_direction()
-
-        t_pos = add(pos, mult(vec, dir))
-        if not self.valid_position(t_pos) or not state.is_enemy(t_pos):
-            return None
-        
-        n_pos = add(pos, mult(vec, dir * 2))
-        if not self.valid_position(n_pos) or not state.is_empty(n_pos):
-            return None
-
-        n_state = state.copy()
-        n_state.remove_piece(pos)
-        n_state.remove_piece(t_pos)
-
-        if self.in_last_row(state, n_pos):
-            self.enter_place_mode(n_state)
-        else:
-            n_state.place_piece(n_pos, state.player)
-            if not (self.can_capture(n_state, dir, n_pos, Direction.WEST) or 
-                self.can_capture(n_state, dir, n_pos, Direction.NORTHWEST) or 
-                self.can_capture(n_state, dir, n_pos, Direction.NORTH) or 
-                self.can_capture(n_state, dir, n_pos, Direction.NORTHEAST) or 
-                self.can_capture(n_state, dir, n_pos, Direction.EAST)):
-                n_state.next_turn()
-            else:
-                n_state.action = Capture(n_pos)
-        
-        return n_state
-
-    def place(self, state: State, pos: tuple) -> State:
-        row = 2 if state.player == 2 else 8
-        if  pos[1] not in range(1, 8) or pos[0] not in range(row - 2, row) or not state.is_empty(pos):
-            return None
-
-        n_state = state.copy()
-        n_state.place_piece(pos, n_state.player)
-        n_state.action.pieces -= 1
-        
-        if n_state.action.pieces == 0:
-            n_state.next_turn()
-        
-        return n_state
-
-    def enter_place_mode(self, state: State) -> None:
-        row = 1 if (state.player == 2) else 7
-        available = state.board[row][1:6].count(0) + state.board[row - 1][1:6].count(0)
-        
-        if available > 1:
-            state.action = Place(2)
-        elif available > 0:
-            state.action = Place(1)
-        else:
-            state.next_turn()
-
-    def player_move(self, state: State) -> State:
+    @staticmethod
+    def player_move(state) -> State:
         n_state = None
         while n_state == None:
 
             if state.action.type == "start":
-                pos = self.sel_cell()
-                if self.check_capture(state):
-                    vec = self.sel_cp_dir()
-                    n_state = self.capture(state, pos, vec)
+                pos = Eximo.sel_cell()
+                if state.check_capture():
+                    vec = Eximo.sel_cp_dir()
+                    n_state = state.capture(pos, vec)
                 else:
-                    vec = self.sel_mv_dir()
-                    n_state = self.move(state, pos, vec) or self.jump(state, pos, vec)
+                    vec = Eximo.sel_mv_dir()
+                    n_state = state.move(pos, vec) or state.jump(pos, vec)
 
             elif state.action.type == "jump":
                 pos = state.action.pos
-                vec = self.sel_mv_dir()
-                n_state = self.jump(state, pos, vec)
+                vec = Eximo.sel_mv_dir()
+                n_state = state.jump(pos, vec)
 
             elif state.action.type == "capture":
                 pos = state.action.pos
-                vec = self.sel_cp_dir()
-                n_state = self.capture(state, pos, vec)
+                vec = Eximo.sel_cp_dir()
+                n_state = state.capture(pos, vec)
 
             elif state.action.type == "place":
-                pos = self.sel_cell()
-                n_state = self.place(state, pos)
+                pos = Eximo.sel_cell()
+                n_state = state.place(pos)
 
         return n_state
 
-    def get_children(self, state: State) -> list:
-
-        ret_states = []
-
-        if state.action.type == "start":
-            # TODO: check if any piece can capture
-            # TODO: if no piece can capture then try jumps and moves for each piece on the board
-
-            regular_arr = []
-
-            for row in range(0,8): 
-                for col in range(0,8):
-                    pos = (row, col)
-
-                    for vec in [Direction.WEST, Direction.EAST]:
-                        n_state = self.capture(state, pos, vec)
-                        if n_state != None: ret_states.append(n_state)
-
-                    for vec in [Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST]:
-                        n_state = self.capture(state, pos, vec)
-                        if n_state != None: ret_states.append(n_state)
-
-                        if ret_states: continue
-                        
-                        n_state = self.move(state, pos, vec) or self.jump(state, pos, vec)
-                        if n_state != None: 
-                            regular_arr.append(n_state)
-
-            if not ret_states:
-                # TODO: check if it is possible to assign
-                ret_states.extend(regular_arr)
-
-        elif state.action.type == "place":
-            # TODO: generate all possible placing possibilities
-            row_c = 0 if (state.player == 2) else 6
-
-            for row in range(0, 2):
-                for col in range(1, 7):
-                    n_state = self.place(state, (row_c + row, col))
-                    if n_state != None: ret_states.append(n_state)
-
-        elif state.action.type == "capture":
-            # TODO: check all the moves that the capturing piece can do
-            for vec in [Direction.WEST, Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST, Direction.EAST]:
-                n_state = self.capture(state, state.action.pos, vec)
-                if n_state != None: ret_states.append(n_state)
-            
-        elif state.action.type == "jump":
-            # TODO: check all the jumps that the jumping piece can do
-            for vec in [Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST]:
-                n_state = self.jump(state, state.action.pos, vec)
-                if n_state != None: ret_states.append(n_state)
-
-        # check if a r_state is a start action, if not get_children of that state until a start state          
-        tmp_states = []
-        for state in ret_states:
-            if state.action.type == "start":
-                continue
-            
-            tmp_states.extend(self.get_children(state))
-            ret_states.remove(state)
-            
-        ret_states.extend(tmp_states)
-        return ret_states
-
-    # TODO: deal with the case when the player has no more moves
-    def minimax(self, state: State, depth: int, max_player: int, root: bool) -> State:
+    @staticmethod
+    def minimax(state: State, depth: int, max_player: int, root: bool) -> State:
         
         if depth == 0: # or no more possible moves
             return state
-        children = self.get_children(state)
+        children = state.get_children()
         if len(children) == 0:
             return state
 
@@ -342,7 +105,7 @@ class Eximo:
             
 
             for i, child in enumerate(children):
-                n_state = self.minimax(child, depth - 1, max_player, False)
+                n_state = Eximo.minimax(child, depth - 1, max_player, False)
                 if n_state.score[max_player] > max_score:
                     max_score = n_state.score[max_player]
                     max_state = n_state
@@ -353,12 +116,12 @@ class Eximo:
                 return children[max_index]
         
         else:
-            min_score = 65
+            min_score = sys.maxsize
             min_state = state
             min_index = -1
 
             for i, child in enumerate(children):
-                n_state = self.minimax(child, depth - 1, max_player, False)
+                n_state = Eximo.minimax(child, depth - 1, max_player, False)
                 if n_state.score[max_player] < min_score:
                     min_score = n_state.score[max_player]
                     min_state = n_state
--- eximo/src/state.py
+++ eximo/src/state.py
@@ -1,5 +1,6 @@
 from colorama import Fore, Back, Style 
 from copy import copy
+from aux import add, mult, Direction
 
 class State:
 
@@ -11,11 +12,203 @@ class State:
         self.score[1] = s1
         self.score[2] = s2
 
+    def game_over(self) -> bool:
+        if self.score[1] <= 0:
+            print("player 2 won")
+            return True
+        elif self.score[2] <= 0:
+            print("player 1 won")
+            return True
+        return False
+
+    @staticmethod
+    def valid_position(pos: tuple) -> bool:
+        return pos[0] in range(0, 8) and pos[1] in range(0, 8)
+
+    def in_last_row(self, pos: tuple) -> bool:
+        return (self.player == 1 and pos[0] == 0) or (self.player == 2 and pos[0] == 7)
+
     def copy(self):
         board = [row.copy() for row in self.board]
         player = self.player + 0
         action = self.action
         return State(board, player, self.score[1], self.score[2], action)
+
+    def get_children(self) -> list:
+
+        ret_states = []
+
+        if self.action.type == "start":
+            # TODO: check if any piece can capture
+            # TODO: if no piece can capture then try jumps and moves for each piece on the board
+
+            regular_arr = []
+
+            for row in range(0,8): 
+                for col in range(0,8):
+                    pos = (row, col)
+
+                    for vec in [Direction.WEST, Direction.EAST]:
+                        n_state = self.capture(pos, vec)
+                        if n_state != None: ret_states.append(n_state)
+
+                    for vec in [Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST]:
+                        n_state = self.capture(pos, vec)
+                        if n_state != None: ret_states.append(n_state)
+
+                        if ret_states: continue
+                        
+                        n_state = self.move(pos, vec) or self.jump(pos, vec)
+                        if n_state != None: 
+                            regular_arr.append(n_state)
+
+            if not ret_states:
+                # TODO: check if it is possible to assign
+                ret_states.extend(regular_arr)
+
+        elif self.action.type == "place":
+            # TODO: generate all possible placing possibilities
+            row_c = 0 if (self.player == 2) else 6
+
+            for row in range(0, 2):
+                for col in range(1, 7):
+                    n_state = self.place((row_c + row, col))
+                    if n_state != None: ret_states.append(n_state)
+
+        elif self.action.type == "capture":
+            # TODO: check all the moves that the capturing piece can do
+            for vec in [Direction.WEST, Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST, Direction.EAST]:
+                n_state = self.capture(self.action.pos, vec)
+                if n_state != None: ret_states.append(n_state)
+            
+        elif self.action.type == "jump":
+            # TODO: check all the jumps that the jumping piece can do
+            for vec in [Direction.NORTHWEST, Direction.NORTH, Direction.NORTHEAST]:
+                n_state = self.jump(self.action.pos, vec)
+                if n_state != None: ret_states.append(n_state)
+
+        # check if a r_state is a start action, if not get_children of that state until a start state          
+        tmp_states = []
+        for state in ret_states:
+            if state.action.type == "start":
+                continue
+            
+            tmp_states.extend(state.get_children())
+            ret_states.remove(state)
+            
+        ret_states.extend(tmp_states)
+        return ret_states
+
+    # ORDINARY MOVE OPERATORS
+    def move(self, pos: tuple, vec: tuple):
+        if not self.is_ally(pos):
+            return None
+        
+        dir = self.move_direction()
+        n_pos = add(pos, mult(vec, dir))
+
+        if not State.valid_position(n_pos):
+            return None
+        if not self.is_empty(n_pos):
+            return None
+        
+        n_state = self.copy()
+        n_state.remove_piece(pos)
+
+        if self.in_last_row(n_pos):
+            n_state.enter_place_mode()
+        else:
+            n_state.place_piece(n_pos, self.player)
+            n_state.next_turn()
+
+        return n_state
+
+    # JUMP MOVE OPERATORS
+    def can_jump(self, dir: int, pos: tuple, vec: tuple) -> bool:
+        t_pos = add(pos, mult(vec, dir))
+        if not State.valid_position(t_pos) or not self.is_ally(t_pos):
+            return False
+        
+        n_pos = add(pos, mult(vec, dir * 2))
+        if not State.valid_position(n_pos) or not self.is_empty(n_pos):
+            return False
+        
+        return True
+
+    def jump(self, pos: tuple, vec: tuple):
+        if not self.is_ally(pos):
+            return None
+
+        dir = self.move_direction()
+
+        t_pos = add(pos, mult(vec, dir))
+        if not State.valid_position(t_pos) or not self.is_ally(t_pos):
+            return None
+        
+        n_pos = add(pos, mult(vec, dir * 2))
+        if not State.valid_position(n_pos) or not self.is_empty(n_pos):
+            return None
+
+        n_state = self.copy()
+        n_state.remove_piece(pos)
+
+        if self.in_last_row(n_pos):
+            n_state.enter_place_mode()
+        else:
+            n_state.place_piece(n_pos, self.player)
+            if  not (n_state.can_jump(dir, n_pos, Direction.NORTHWEST) or 
+                n_state.can_jump(dir, n_pos, Direction.NORTH) or 
+                n_state.can_jump(dir, n_pos, Direction.NORTHEAST)):
+                n_state.next_turn()
+            else:
+                n_state.action = Jump(n_pos)
+
+        return n_state
+
+    # CAPTURE OPERATORS
+    def can_capture(self, dir: int, pos: tuple, vec: tuple) -> bool:
+        t_pos = add(pos, mult(vec, dir))
+        if not self.valid_position(t_pos) or not self.is_enemy(t_pos):
+            return False
+        
+        n_pos = add(pos, mult(vec, dir * 2))
+        if not self.valid_position(n_pos) or not self.is_empty(n_pos):
+            return False
+        
+        return True
+
+    def capture(self, pos: tuple, vec: tuple):
+        if not self.is_ally(pos):
+            return None
+        
+        dir = self.move_direction()
+
+        t_pos = add(pos, mult(vec, dir))
+        if not self.valid_position(t_pos) or not self.is_enemy(t_pos):
+            return None
+        
+        n_pos = add(pos, mult(vec, dir * 2))
+        if not self.valid_position(n_pos) or not self.is_empty(n_pos):
+            return None
+
+        n_state = self.copy()
+        n_state.remove_piece(pos)
+        n_state.remove_piece(t_pos)
+
+        if self.in_last_row(n_pos):
+            n_state.enter_place_mode()
+        else:
+            n_state.place_piece(n_pos, self.player)
+            if not (n_state.can_capture(dir, n_pos, Direction.WEST) or 
+                n_state.can_capture(dir, n_pos, Direction.NORTHWEST) or 
+                n_state.can_capture(dir, n_pos, Direction.NORTH) or 
+                n_state.can_capture(dir, n_pos, Direction.NORTHEAST) or 
+                n_state.can_capture(dir, n_pos, Direction.EAST)):
+                n_state.next_turn()
+            else:
+                n_state.action = Capture(n_pos)
+        
+        return n_state
 
     def print(self):
         print()
@@ -43,7 +236,7 @@ class State:
         self.player = self.player % 2 + 1
         self.action = Start()
 
-    def get_piece(self, pos: tuple) -> bool:
+    def get_piece(self, pos: tuple) -> int:
         return self.board[pos[0]][pos[1]]
 
     def is_empty(self, pos: tuple) -> bool:
@@ -68,6 +261,45 @@ class State:
     def move_direction(self) -> int:
         return 1 if self.player == 2 else -1
 
+    def check_capture(self) -> bool:
+        dir = self.move_direction()
+        for row in range(len(self.board)):
+            for col in range(len(self.board[row])):
+                if not self.is_ally((row, col)):
+                    continue
+
+                if (self.can_capture(dir, (row, col), Direction.WEST) or 
+                    self.can_capture(dir, (row, col), Direction.NORTHWEST) or 
+                    self.can_capture(dir, (row, col), Direction.NORTH) or 
+                    self.can_capture(dir, (row, col), Direction.NORTHEAST) or 
+                    self.can_capture(dir, (row, col), Direction.EAST)):
+                    return True
+        return False
+
+    def place(self, pos: tuple):
+        row = 2 if self.player == 2 else 8
+        if  pos[1] not in range(1, 8) or pos[0] not in range(row - 2, row) or not self.is_empty(pos):
+            return None
+
+        n_state = self.copy()
+        n_state.place_piece(pos, n_state.player)
+        n_state.action.pieces -= 1
+        
+        if n_state.action.pieces == 0:
+            n_state.next_turn()
+        
+        return n_state
+
+    def enter_place_mode(self) -> None:
+        row = 1 if (self.player == 2) else 7
+        available = self.board[row][1:6].count(0) + self.board[row - 1][1:6].count(0)
+        
+        if available > 1:
+            self.action = Place(2)
+        elif available > 0:
+            self.action = Place(1)
+        else:
+            self.next_turn()
 
 class Start:
     type = "start"
@@ -92,3 +324,12 @@ class Place:
     def __init__(self, pieces):
         self.pieces = pieces
 
+start_state = State([
+        [0, 2, 2, 2, 2, 2, 2, 0],
+        [0, 2, 2, 2, 2, 2, 2, 0], 
+        [0, 2, 2, 0, 0, 2, 2, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 0, 0, 0, 0, 0, 0, 0], 
+        [0, 1, 1, 0, 0, 1, 1, 0], 
+        [0, 1, 1, 1, 1, 1, 1, 0], 
+        [0, 1, 1, 1, 1, 1, 1, 0]], 1, 16, 16, Start())
