c58042b39ebc1f654ff43aa77d6304bf5f87f645
Vitor Hugo <vhlg99@gmail.com>
2020-03-31 21:13:29 +0100

    Added comments to aux.py

--- eximo/src/aux.py
+++ eximo/src/aux.py
@@ -1,11 +1,15 @@
 
+# addition of two tuples
+# ex: (1, 4) + (3, 2) = (4, 6)
 def add(t1: tuple, t2: tuple) -> tuple:
     return (t1[0] + t2[0], t1[1] + t2[1])
 
+# multiply a tuple by a constant
+# (2, 3) * 2 = (4, 6)
 def mult(t: tuple, f: int) -> tuple:
     return (t[0] * f, t[1] * f)
 
-
+# a simple enum for referencing move directions
 class Direction:
     WEST = (0, 1)
     NORTHWEST = (1, 1)
